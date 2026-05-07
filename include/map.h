6add939cf2399c55d9b572b647a40db4c1f4658f
Arthur Moura <arthur.bertolini.moura@gmail.com>
2026-05-07 19:08:36 -0300

    Update header files

--- include/map.h
+++ include/map.h
@@ -1,6 +1,8 @@
 #ifndef MAP_H__
 #define MAP_H__
 
+#include "grafo.h"
+
 class Map {
 private:
 public:
--- include/map_generator.h
+++ include/map_generator.h
@@ -2,23 +2,28 @@
 #define MAP_GENERATOR_H__
 
 #include <string>
+#include <fstream>
 #include "map.h"
 
 class MapGenerator {
 private:
+    // Nome do arquivo de texto onde mapa deverá estar armazenado
     std::string filename_;
+
+    // Objeto mapa construído a partir da leitura do arquivo
     Map mapa_;
 
     Map build_from_csv();
 
-    Map build_from_jsom();
-    
+    Map build_from_json();
 public:
     MapGenerator(std::string path_to_file);
     
     ~MapGenerator();
 
     Map map();
+
+    const std::string filename();
 };
 
 #endif
