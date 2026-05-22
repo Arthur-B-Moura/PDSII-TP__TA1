304ecc61c5dff489958ebb0a4ba5002ca8c0e020
Arthur Moura <arthur.bertolini.moura@gmail.com>
2026-05-21 23:15:23 -0300

    Add: adiciona condições de teste

--- test/test_map_node.cpp
+++ test/test_map_node.cpp
@@ -4,10 +4,30 @@
 #include "catch2/catch.hpp"
 #include "map_node.h"
 
+bool todos_pertencem(std::vector<long long int> ids, MapNode m) {
+    for (const auto& i : ids) {
+        if (!m.eh_vizinho(i)) return false;
+    }
+    return true;
+}
+
 TEST_CASE("Inicialização simples do objeto") {
     MapNode m(5);
 
     REQUIRE(m.id() == 5);
     REQUIRE(m.posicao() == std::nullopt);
     REQUIRE(m.eh_vizinho(5) == false);
+};
+
+TEST_CASE("Adiciona serie de vizinhos") {
+    std::vector<long long int> v = {4,6,1,3,56,8,235,13,7,4,1};
+    MapNode n(4);
+
+    n.add_vizinho(4);
+    CHECK(n.eh_vizinho(4) == true);
+
+    SECTION("Adicionando vetor de vizinhos após add 1") {
+        n.add_vizinho(v);
+        CHECK(todos_pertencem(v, n) == true);
+    }
 };
