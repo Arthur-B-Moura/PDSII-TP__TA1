4767f4e5eabd2b498a91e7bc58f1a9f3942dd6ce
heitoraugustoPeenngguuiinn <heitoraugustoamorim@gmail.com>
2026-06-11 11:26:06 -0300

    Teste simples de compilação

---
+++ src/main.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <string>
+#include "grafo.h"
+#include "map_node.h"
+#include "map_generator.h"
+#include "map.h"
+#include "pathfinder.h"
+
+//esse hello world é só um placeholder, foi  só um teste de compilação
+
+int main() {
+    std::cout << "Hello, World!" << std::endl;
+    return 0;
+}
